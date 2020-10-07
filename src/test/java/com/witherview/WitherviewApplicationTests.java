ad78ca43a575e851a230cce829b745b43b82df8f
sjh2428 <pok_gare@naver.com>
2020-10-07 22:47:45 +0900

    env: 초기 개발 세팅

---
+++ src/test/java/com/witherview/WitherviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.witherview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitherviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
