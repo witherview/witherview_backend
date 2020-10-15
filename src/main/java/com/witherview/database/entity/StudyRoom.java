256b6ea7c2aee81ecc4902fb2b86920bd182f066
sjh2428 <pok_gare@naver.com>
2020-10-15 20:20:32 +0900

    feat(entity): 스터디룸 Entity/Repository 추가

---
+++ src/main/java/com/witherview/database/entity/StudyRoom.java
@@ -0,0 +1,41 @@
+package com.witherview.database.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity @Getter
+@NoArgsConstructor
+@Table(name = "tbl_study_room")
+public class StudyRoom {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User host;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String title;
+
+    @NotNull
+    @Column(nullable = false)
+    private Byte nowUserCnt;
+
+    @NotNull
+    @Column(nullable = false)
+    private Byte maxUserCnt;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate date;
+
+    @Column(columnDefinition = "TIME")
+    private LocalTime time;
+}
--- src/main/java/com/witherview/database/entity/User.java
+++ src/main/java/com/witherview/database/entity/User.java
@@ -52,4 +52,7 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<QuestionList> questionLists = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<StudyRoom> studyRooms = new ArrayList<>();
 }
