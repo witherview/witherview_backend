b5abedcdb43db4a3cb478a797e844520da5738a5
sjh2428 <pok_gare@naver.com>
2020-10-15 19:20:38 +0900

    feat(entity): 유저 Entity/Repository 추가

---
+++ src/main/java/com/witherview/database/entity/User.java
@@ -0,0 +1,50 @@
+package com.witherview.database.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity @Getter
+@NoArgsConstructor
+@Table(name = "tbl_user")
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    private String refreshToken;
+
+    private String mainIndustry;
+
+    private String subIndustry;
+
+    private String mainJob;
+
+    private String subJob;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Integer studyCnt;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Integer selfPracticeCnt;
+
+    @Column(nullable = false)
+    private Byte reliability;
+}
---
+++ src/main/java/com/witherview/database/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.witherview.database.repository;
+
+import com.witherview.database.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
