f83e9feb851b47457c20cbb06fc3f85f2931bb70
sjh2428 <pok_gare@naver.com>
2020-10-21 19:31:37 +0900

    feat(bean): model mapper 빈 등록

--- src/main/java/com/witherview/WitherviewApplication.java
+++ src/main/java/com/witherview/WitherviewApplication.java
@@ -1,5 +1,6 @@
 package com.witherview;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,10 @@ public class WitherviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WitherviewApplication.class, args);
+	}
+
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
