ba61963e14633d6b06d9e93a8b6e32d061f08cb7
sjh2428 <pok_gare@naver.com>
2020-11-01 20:17:18 +0900

    fix(test): test supporter에 어노테이션 빠진것 수정

--- src/test/java/com/witherview/support/TestSupporter.java
+++ src/test/java/com/witherview/support/TestSupporter.java
@@ -1,10 +1,12 @@
 package com.witherview.support;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@SpringBootTest
 public abstract class TestSupporter {
 }
