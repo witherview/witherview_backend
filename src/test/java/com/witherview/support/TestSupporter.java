ac80461365f6fef0ee654c765bfc12b7c5fcad75
sjh2428 <pok_gare@naver.com>
2020-11-01 16:50:14 +0900

    feat(test): test supporter 추가

---
+++ src/test/java/com/witherview/support/TestSupporter.java
@@ -0,0 +1,10 @@
+package com.witherview.support;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public abstract class TestSupporter {
+}
