2ec86cf20f9eb0fa37b8e2ae5d5106b8a9776b9a
MoonYeeun <aams82@hanmail.net>
2020-11-03 02:19:08 +0900

    feat(repository): 유저아이디로 질문리스트들 조회하는 메소드 추가

--- src/main/java/com/witherview/database/repository/QuestionListRepository.java
+++ src/main/java/com/witherview/database/repository/QuestionListRepository.java
@@ -3,5 +3,8 @@ package com.witherview.database.repository;
 import com.witherview.database.entity.QuestionList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionListRepository extends JpaRepository<QuestionList, Long> {
+    List<QuestionList> findAllByOwnerId(Long ownerId);
 }
