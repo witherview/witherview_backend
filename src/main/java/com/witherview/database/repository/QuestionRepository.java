d2984f0840b7cb0a1934a3345a0517a5f105a135
MoonYeeun <aams82@hanmail.net>
2020-11-07 03:41:44 +0900

    feat(repository/Question): 질문리스트 아이디로 질문들 조회하는 메소드 추가

--- src/main/java/com/witherview/database/repository/QuestionRepository.java
+++ src/main/java/com/witherview/database/repository/QuestionRepository.java
@@ -3,5 +3,8 @@ package com.witherview.database.repository;
 import com.witherview.database.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findAllByBelongListId(Long belongListId);
 }
