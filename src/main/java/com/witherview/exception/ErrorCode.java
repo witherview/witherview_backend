7d67215fa00cc2328af1383aa652839dbd1b2911
MoonYeeun <aams82@hanmail.net>
2020-11-08 14:28:45 +0900

    fix(exception): 응답 코드 수정 없는 자원을 요청한 경우이므로 404로 변경

--- src/main/java/com/witherview/exception/ErrorCode.java
+++ src/main/java/com/witherview/exception/ErrorCode.java
@@ -17,9 +17,9 @@ public enum ErrorCode {
     NOT_EQUAL_PASSWORD(400, "AUTH002", "입력하신 두 비밀번호가 일치하지 않습니다."),
 
     // Self-Practice
-    NOT_FOUND_USER(400, "SELF-PRACTICE001", "해당 유저가 없습니다."),
-    NOT_FOUND_QUESTIONLIST(400, "SELF-PRACTICE002", "해당 질문리스트가 없습니다."),
-    NOT_FOUND_QUESTION(400, "SELF-PRACTICE003", "해당 질문이 없습니다.");
+    NOT_FOUND_USER(404, "SELF-PRACTICE001", "해당 유저가 없습니다."),
+    NOT_FOUND_QUESTIONLIST(404, "SELF-PRACTICE002", "해당 질문리스트가 없습니다."),
+    NOT_FOUND_QUESTION(404, "SELF-PRACTICE003", "해당 질문이 없습니다.");
 
     private final int status;
     private final String code;
