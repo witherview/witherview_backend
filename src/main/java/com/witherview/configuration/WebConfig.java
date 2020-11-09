67944bd3b4c59e92fa523579884dcd4a90642c52
sjh2428 <pok_gare@naver.com>
2020-11-09 16:50:36 +0900

    fix(interceptor): 인터셉터 부분 분리

--- src/main/java/com/witherview/configuration/WebConfig.java
+++ src/main/java/com/witherview/configuration/WebConfig.java
@@ -1,6 +1,5 @@
 package com.witherview.configuration;
 
-import com.witherview.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -29,12 +27,5 @@ public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcCon
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor())
-                .addPathPatterns("/**")
-                .excludePathPatterns("/login", "/register");
     }
 }
