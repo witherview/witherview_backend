4c1d95782e092df0da813fc141ca511f871cd6a1
sjh2428 <pok_gare@naver.com>
2020-11-11 16:11:18 +0900

    feat(test/login): 로그인 없이 세션이 필요한 API 접근시 401에러 발생
    - 예은님 코드 머지되면 이 부분은 현재 404로 뜨지만 401 에러가 뜰 예정

--- src/test/java/com/witherview/account/LoginControllerTest.java
+++ src/test/java/com/witherview/account/LoginControllerTest.java
@@ -76,4 +76,16 @@ public class LoginControllerTest extends AccountSupporter {
         resultActions.andExpect(jsonPath("$.status").value(404));
         resultActions.andExpect(jsonPath("$.code").value("AUTH003"));
     }
+
+    @Test
+    public void 로그인_없이_API_접근() throws Exception {
+        ResultActions resultActions = mockMvc.perform(post("/self/question")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+
+        resultActions.andExpect(jsonPath("$.status").value(401));
+        resultActions.andExpect(jsonPath("$.code").value("AUTH004"));
+    }
 }
