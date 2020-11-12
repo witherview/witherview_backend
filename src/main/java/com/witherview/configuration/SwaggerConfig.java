2e0e11ba7bc1c54359413e85399d0f89cf8966c2
MoonYeeun <aams82@hanmail.net>
2020-11-13 00:26:18 +0900

    feat(config): swagger 적용을 위한 config 파일 추가

---
+++ src/main/java/com/witherview/configuration/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.witherview.configuration;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.RequestHandlerSelectors.withMethodAnnotation;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    private String title;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket apiV1() {
+        title = "Witherview API ";
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo(title))
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo(String title) {
+        return new ApiInfoBuilder()
+                .title(title)
+                .description("Witherview API DOCS")
+                .build();
+    }
+}
