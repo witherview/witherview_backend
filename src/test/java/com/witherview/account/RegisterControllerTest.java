6aba58a4bf399efc48329fc1873c2ede8e8ec8cb
sjh2428 <pok_gare@naver.com>
2020-11-18 16:07:31 +0900

    fix(test/account): 계정 관련 테스트 코드 수정
    - 트랜잭션 추가
    - 트랜잭션을 추가하면 롤백되기 때문에 null 체크 삭제
    - api 주소 수정

--- src/test/java/com/witherview/account/RegisterControllerTest.java
+++ src/test/java/com/witherview/account/RegisterControllerTest.java
@@ -4,12 +4,14 @@ import com.witherview.database.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 public class RegisterControllerTest extends AccountSupporter {
 
     @Test
