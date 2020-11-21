1e5add5fd8d22cc9c4ff85eda3c0dfbb2f666933
sjh2428 <pok_gare@naver.com>
2020-11-21 13:55:39 +0900

    fix(cors): hotfix! cors!

--- src/main/java/com/witherview/configuration/WebConfig.java
+++ src/main/java/com/witherview/configuration/WebConfig.java
@@ -7,10 +7,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebConfig extends WebSecurityConfigurerAdapter {
+public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
