961987e8f5291285e728cbc78188d8065db85837
MoonYeeun <aams82@hanmail.net>
2020-11-28 22:14:26 +0900

    fix(entity) : base entity에 수정 시간 칼럼 추가

--- src/main/java/com/witherview/database/entity/CreatedBaseEntity.java
+++ src/main/java/com/witherview/database/entity/CreatedBaseEntity.java
@@ -2,10 +2,12 @@ package com.witherview.database.entity;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
 import java.time.LocalDateTime;
 
 @Getter @MappedSuperclass
@@ -14,4 +16,7 @@ public abstract class CreatedBaseEntity {
 
     @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 }
