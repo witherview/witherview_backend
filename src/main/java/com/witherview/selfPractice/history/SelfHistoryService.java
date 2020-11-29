5bc1183fb5ab6e438cfd580e1ef36f2ad4dd02a8
sjh2428 <pok_gare@naver.com>
2020-11-29 15:23:52 +0900

    feat(self/history): 파일 위치를 저장할 컬럼 및 로직 추가

--- src/main/java/com/witherview/selfPractice/history/SelfHistoryService.java
+++ src/main/java/com/witherview/selfPractice/history/SelfHistoryService.java
@@ -1,5 +1,6 @@
 package com.witherview.selfPractice.history;
 
+import com.witherview.account.AccountSession;
 import com.witherview.database.entity.QuestionList;
 import com.witherview.database.entity.SelfHistory;
 import com.witherview.database.entity.User;
@@ -10,9 +11,11 @@ import com.witherview.exception.BusinessException;
 import com.witherview.exception.ErrorCode;
 import com.witherview.selfPractice.exception.NotFoundQuestionList;
 import com.witherview.selfPractice.exception.NotFoundUser;
+import com.witherview.video.VideoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -23,11 +26,12 @@ public class SelfHistoryService {
     private final UserRepository userRepository;
     private final QuestionListRepository questionListRepository;
     private final SelfHistoryRepository selfHistoryRepository;
+    private final VideoService videoService;
 
     @Transactional
-    public SelfHistory save(SelfHistoryDTO.SelfHistorySaveDTO dto, Long userId) {
-        User user = userRepository.findById(userId).orElseThrow(NotFoundUser::new);
-        QuestionList questionList = questionListRepository.findById(dto.getQuestionListId())
+    public SelfHistory save(MultipartFile videoFile, Long questionListId, AccountSession accountSession) {
+        User user = userRepository.findById(accountSession.getId()).orElseThrow(NotFoundUser::new);
+        QuestionList questionList = questionListRepository.findById(questionListId)
                                     .orElseThrow(NotFoundQuestionList::new);
         if (!user.getId().equals(questionList.getOwner().getId())) {
             throw new BusinessException(ErrorCode.NOT_FOUND_QUESTIONLIST);
@@ -35,7 +39,11 @@ public class SelfHistoryService {
 
         SelfHistory selfHistory = new SelfHistory(questionList);
         user.addSelfHistory(selfHistory);
-        return selfHistoryRepository.save(selfHistory);
+        selfHistoryRepository.save(selfHistory);
+        String savedLocation = videoService.upload(videoFile,
+                                           accountSession.getEmail() + "/self/" + selfHistory.getId());
+        selfHistory.updateSavedLocation(savedLocation);
+        return selfHistory;
     }
 
     public List<SelfHistory> findAll(Long userId) {
