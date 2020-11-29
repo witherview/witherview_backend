dfa338b57da1754eef50ffd017c3068aa76e24b2
MoonYeeun <aams82@hanmail.net>
2020-11-30 02:47:06 +0900

    fix(repository) : StudyRoomParticipantRepository 수정 - 방 id와 user id로 삭제하는 쿼리메소드 추가

--- src/main/java/com/witherview/database/repository/StudyRoomParticipantRepository.java
+++ src/main/java/com/witherview/database/repository/StudyRoomParticipantRepository.java
@@ -3,5 +3,10 @@ package com.witherview.database.repository;
 import com.witherview.database.entity.StudyRoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudyRoomParticipantRepository extends JpaRepository<StudyRoomParticipant, Long> {
+    List<StudyRoomParticipant> findByStudyRoomId(Long studyRoomId);
+
+    void deleteByStudyRoomIdAndUserId(Long studyRoomId, Long userId);
 }
