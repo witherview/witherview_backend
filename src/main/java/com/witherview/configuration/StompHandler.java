cb2774cbe5b149692f50b8b3056264ab9b1412fd
yeemoon <yeemoon@ebay.com>
2020-12-06 06:45:17 +0900

    feat(config): 존재하지 않는 방 구독 시 예외처리를 위한 핸들러 추가

---
+++ src/main/java/com/witherview/configuration/StompHandler.java
@@ -0,0 +1,36 @@
+package com.witherview.configuration;
+
+import com.witherview.groupPractice.GroupStudyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessagingException;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class StompHandler implements ChannelInterceptor {
+    private final GroupStudyService groupStudyService;
+
+    // websocket을 통해 들어온 요청이 처리 되기전 실행된다.
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        // websocket 연결 후 subscribe 시 존재하는 방 구독하는지 체크
+        if(StompCommand.SUBSCRIBE == accessor.getCommand()) {
+            String room = message.getHeaders().get("simpDestination").toString().split("/")[3];
+            Long roomId = Long.parseLong(room);
+            try {
+                groupStudyService.findRoom(roomId);
+            } catch (Exception e) {
+                throw new MessagingException(e.getMessage());
+            }
+        }
+        return message;
+    }
+}
