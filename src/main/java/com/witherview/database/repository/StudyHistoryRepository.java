c9b9a585b571501309db3eb50a3c967ff94abf12
yeemoon <yeemoon@ebay.com>
2021-01-26 23:14:27 +0900

    feat(repository): 스터디 내역 및 피드백 채팅 내역 레포지토리 추가

---
+++ src/main/java/com/witherview/database/repository/StudyHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.witherview.database.repository;
+
+import com.witherview.database.entity.StudyHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyHistoryRepository extends JpaRepository<StudyHistory, Long> {
+}
