f55541e3536845b3561f3fe7d14e5c243bf69739
yeemoon <yeemoon@ebay.com>
2021-01-26 23:35:00 +0900

    feat(Redis): redis pub/sub 을 위한 RedisSubscriber 추가

---
+++ src/main/java/com/witherview/chat/RedisSubscriber.java
@@ -0,0 +1,24 @@
+package com.witherview.chat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class RedisSubscriber {
+    private final ObjectMapper objectMapper;
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    public void sendMessage(String message) {
+        try {
+            ChatDTO.FeedBackDTO feedBackDTO = objectMapper.readValue(message, ChatDTO.FeedBackDTO.class);
+            messagingTemplate.convertAndSend("/sub/feedback/" + feedBackDTO.getStudyHistoryId(), message);
+        } catch (Exception e) {
+            log.error("Exception {}", e);
+        }
+    }
+}
