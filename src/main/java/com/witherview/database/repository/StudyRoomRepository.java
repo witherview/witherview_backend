63e4eabf23944eb1be93f083bf7c60cd66601bad
MoonYeeun <aams82@hanmail.net>
2021-01-29 03:06:46 +0900

    feat(repository/groupStudy) : 카테고리 별 스터디룸 조회하는 쿼리메소드 추가

--- src/main/java/com/witherview/database/repository/StudyRoomRepository.java
+++ src/main/java/com/witherview/database/repository/StudyRoomRepository.java
@@ -1,7 +1,11 @@
 package com.witherview.database.repository;
 
 import com.witherview.database.entity.StudyRoom;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudyRoomRepository extends JpaRepository<StudyRoom, Long> {
+    List<StudyRoom> findAllByCategory(Pageable pageable, String category);
 }
