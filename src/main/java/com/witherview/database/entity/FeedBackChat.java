db42aba1f931a2be817d11be0479ae60069c1043
inspirit941 <inspirit941@gmail.com>
2021-02-07 19:58:23 +0900

    refactor(mongoDB) : Document 생성

--- src/main/java/com/witherview/database/entity/FeedBackChat.java
+++ src/main/java/com/witherview/database/entity/FeedBackChat.java
@@ -3,46 +3,34 @@ package com.witherview.database.entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
-@Entity
-@Getter
-@NoArgsConstructor
-@Table(name = "tbl_feedback_chat")
+
+@Getter @NoArgsConstructor @Builder
+@Document
+@CompoundIndex(def = "{'writtenUserId': 1 , 'studyHistoryId': -1, 'targetUserId': 1}")
 public class FeedBackChat {
     @Id
-    @GeneratedValue
-    private Long id;
+    private String id;
+    // compoundKey
+    private Long targetUserId;
+    private Long writtenUserId;
+    private Long studyHistoryId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "target_user_id", nullable = false)
-    private User targetUser;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "written_user_id", nullable = false)
-    private User writtenUser;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "study_history_id", nullable = false)
-    private StudyHistory studyHistory;
-
+    private String targetUserName;
+    private String writtenUserName;
     @NotBlank
-    @Column(columnDefinition = "TEXT", nullable = false)
     private String message;
 
-    @NotNull @Column(nullable = false)
-    private LocalDateTime createdAt;
+    private LocalTime createdAt; // 영상 시작하고 몇 분에 달린 comment인지.
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
 
-    @Builder
-    public FeedBackChat(StudyHistory studyHistory, User writtenUser, User targetUser, String message, LocalDateTime createdAt) {
-        this.studyHistory = studyHistory;
-        this.writtenUser = writtenUser;
-        this.targetUser = targetUser;
-        this.message = message;
-        this.createdAt = createdAt;
-    }
 }
