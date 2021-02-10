403e320d08ebe5041a5d3215d2257985f7323026
inspirit941 <inspirit941@gmail.com>
2021-02-10 21:12:34 +0900

    feat(config/WebSocket) : BrokeRelay 적용 - 웹소켓의 tcp 요청을 rabbitmq topic으로 relay하기 위한 설정

--- src/main/java/com/witherview/configuration/WebSocketConfig.java
+++ src/main/java/com/witherview/configuration/WebSocketConfig.java
@@ -1,12 +1,15 @@
 package com.witherview.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.concurrent.BlockingDeque;
 
 @RequiredArgsConstructor
 @Configuration
@@ -25,8 +28,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
         registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableStompBrokerRelay("/topic")
+                .setRelayHost("localhost")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest")
+        ;
+
     }
 
     @Override
