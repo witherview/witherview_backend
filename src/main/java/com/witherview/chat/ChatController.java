ba377b0d18ca7174ae0159c7b9b2df866f6e7a9b
inspirit941 <inspirit941@gmail.com>
2021-02-14 17:02:40 +0900

    fix(chat) : controller 수정. - controller Swagger 가독성 향상 - 리턴객체를 modelmapper 적용하는 로직 -> service에서 작업하도록 변경.

--- src/main/java/com/witherview/chat/ChatController.java
+++ src/main/java/com/witherview/chat/ChatController.java
@@ -4,10 +4,10 @@ import com.google.gson.Gson;
 import com.witherview.account.AccountSession;
 import com.witherview.database.entity.FeedBackChat;
 import com.witherview.exception.ErrorResponse;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.http.HttpStatus;
@@ -29,13 +29,13 @@ import static com.witherview.exception.ErrorCode.INVALID_INPUT_VALUE;
 
 @RequiredArgsConstructor
 @Controller
+@Api(value = "채팅 API", description = "서버에 있는 채팅기록 DB로 저장 및 조회", tags = "채팅 API")
 public class ChatController {
     private final ChatService chatService;
     private final RedisTemplate redisTemplate;
     private final ChannelTopic channelTopic;
     private final Gson gson;
     private final SimpMessageSendingOperations messagingTemplate;
-    private final ModelMapper modelMapper;
 
     @MessageMapping("/chat.room")
     public void message(@Payload ChatDTO.MessageDTO message) {
@@ -54,26 +54,38 @@ public class ChatController {
     }
 
     @ApiOperation(value="그룹스터디 피드백 내용 등록")
-    @PostMapping(path = "/api/message/feedback", consumes = MediaType.APPLICATION_JSON_VALUE,
+    @ApiResponses({
+            @ApiResponse(
+                    code = 201,
+                    message = "Created"
+//                    response = ChatDTO.FeedBackDTO.class,
+//                    responseContainer = "List"
+            )
+    })
+    @PostMapping(path = "/api/messages/feedbacks", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> saveFeedBackMessage(@RequestBody @Valid ChatDTO.SaveDTO requestDto,
                                   BindingResult result) {
-        if(result.hasErrors()) {
-            ErrorResponse errorResponse = ErrorResponse.of(INVALID_INPUT_VALUE, result);
-            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-        }
-        List<FeedBackChat> lists = chatService.saveFeedbackMessage(requestDto);
-        return new ResponseEntity<>(modelMapper.map(lists, ChatDTO.SavedFeedBackDTO[].class), HttpStatus.CREATED);
+        if(result.hasErrors())
+            return new ResponseEntity<>(ErrorResponse.of(INVALID_INPUT_VALUE, result), HttpStatus.BAD_REQUEST);
+        chatService.saveFeedbackMessage(requestDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @ApiOperation(value="그룹스터디 피드백 내용 조회")
-    @GetMapping(path = "/api/message/feedback")
-    public ResponseEntity<?> getFeedBackMessage(@ApiParam(value = "조회할 스터디 내역 id") @RequestParam(value = "historyId") Long historyId,
-                                                @ApiParam(value = "조회할 피드백 메세지 idx (디폴트 값 = 0)")
-                                                @RequestParam(value = "idx", required = false) Integer current,
-                                                @ApiIgnore HttpSession session) {
+    @ApiOperation(value="그룹스터디 피드백 내용 조회. 현재 버전은 세션에서 userId값을 가져와 사용한다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "historyId", value = "조회할 스터디 내역 Id"),
+            @ApiImplicitParam(name = "idx", value = "조회할 피드백 메세지 pagination idx (디폴트 값 = 0), 현재 10개 단위")
+    })
+    @GetMapping(path = "/api/messages/feedbacks")
+    public ResponseEntity<List<ChatDTO.FeedBackDTO>> getFeedBackMessage(
+            @RequestParam(value = "historyId") Long historyId,
+            @RequestParam(value = "idx", required = false) Integer idx,
+            @ApiIgnore HttpSession session) {
+
         AccountSession accountSession = (AccountSession) session.getAttribute("user");
-        List<FeedBackChat> lists = chatService.getFeedbackMessage(historyId, accountSession.getId(), current);
-        return new ResponseEntity<>(modelMapper.map(lists, ChatDTO.SavedFeedBackDTO[].class), HttpStatus.OK);
+
+        var lists = chatService.getFeedbackMessageByReceivedUserId(accountSession.getId(), historyId, idx);
+        return new ResponseEntity<>(lists, HttpStatus.OK);
     }
 }
