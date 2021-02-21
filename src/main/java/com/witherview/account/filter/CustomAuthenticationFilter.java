cd898f4e3792c9f2947ba9390daeaa77d8d466f8
inspirit941 <inspirit941@gmail.com>
2021-02-22 01:08:00 +0900

    feat(authentication) : jwt 생성하는 filter 추가

---
+++ src/main/java/com/witherview/account/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.witherview.account.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.witherview.account.AccountDTO;
+import com.witherview.account.exception.InvalidLoginException;
+import com.witherview.constant.SecurityConstant;
+import com.witherview.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    // Authentication 시도
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            var credentials = new ObjectMapper().readValue(request.getInputStream(), AccountDTO.LoginDTO.class);
+            // loadUserByUsername 실행하는 영역
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            credentials.getEmail(), // 아이디
+                            credentials.getPassword(), // 패스워드
+                            new ArrayList<>() // 권한
+                    )
+            );
+        } catch (IOException e) {
+            throw new InvalidLoginException();
+        }
+    }
+
+    // 로그인 성공 시 호출
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        // 인증 통과한 사용자 이름 가져오기
+        String email = ((User) authResult.getPrincipal()).getUsername();
+
+        String token = new JwtUtils().createToken(email);
+
+        // response header
+        response.addHeader(SecurityConstant.AUTHORIZATION_HEADER, SecurityConstant.TOKEN_PREFIX + token);
+    }
+}
