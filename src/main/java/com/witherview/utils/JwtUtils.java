f9300eb2ddf860b30e6df76e8b168ba31de417bb
inspirit941 <inspirit941@gmail.com>
2021-02-22 01:06:39 +0900

    feat(jwt) : jwt 생성을 위한 객체 / 변수 생성

---
+++ src/main/java/com/witherview/utils/JwtUtils.java
@@ -0,0 +1,41 @@
+package com.witherview.utils;
+
+import com.witherview.constant.SecurityConstant;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtils {
+    private final Key key;
+    public JwtUtils() {
+        this.key = Keys.hmacShaKeyFor(SecurityConstant.SECRET.getBytes());
+    }
+    // input 값으로 jwt 토큰 생성하기.
+    public String createToken(String email) {
+
+        var token = Jwts.builder()
+                .claim("email", email)
+                .setExpiration(new Date(System.currentTimeMillis()+ SecurityConstant.EXPIRATION_TIME)) // 1시간)
+                .signWith(this.key, SignatureAlgorithm.HS256)
+                .compact()
+                ;
+
+        return token;
+    }
+
+    public Claims getClaims(String token) {
+//        Key key = Keys.hmacShaKeyFor(SecurityConstant.SECRET.getBytes());
+        var claims = Jwts.parserBuilder()
+                .setSigningKey(key).build()
+                .parseClaimsJwt(token).getBody();
+        return claims;
+    }
+
+}
