91068a4fa6179d028ed6ad71f33c4c7b3782bd22
inspirit941 <inspirit941@gmail.com>
2021-02-24 15:39:07 +0900

    feat(account) : Mapstruct로 mapper 변

---
+++ src/main/java/com/witherview/utils/AccountMapper.java
@@ -0,0 +1,17 @@
+package com.witherview.utils;
+
+import com.witherview.account.AccountDTO;
+import com.witherview.database.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface AccountMapper {
+
+    AccountDTO.ResponseRegister toRegister(User user);
+    AccountDTO.ResponseMyInfo toResponseMyInfo(User user);
+    AccountDTO.UpdateMyInfoDTO toUpdateMyInfo(User user);
+    AccountDTO.UploadProfileDTO toUploadProfile(User user);
+
+}
