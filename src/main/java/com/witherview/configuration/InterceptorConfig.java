042865aea4674ce448a0365a0de3757afa52490e
inspirit941 <inspirit941@gmail.com>
2021-02-24 15:40:36 +0900

    fix(jwt) : 세션기반 Interceptor 비활성화

--- src/main/java/com/witherview/configuration/InterceptorConfig.java
+++ src/main/java/com/witherview/configuration/InterceptorConfig.java
@@ -1,19 +1,19 @@
-package com.witherview.configuration;
-
-import com.witherview.interceptor.AuthInterceptor;
-import com.witherview.interceptor.ResourceAuthInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class InterceptorConfig implements WebMvcConfigurer {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor())
-                .addPathPatterns("/api/**");
-
-        registry.addInterceptor(new ResourceAuthInterceptor())
-                .addPathPatterns("/videos/**");
-    }
-}
+//package com.witherview.configuration;
+//
+//import com.witherview.interceptor.AuthInterceptor;
+//import com.witherview.interceptor.ResourceAuthInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class InterceptorConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new AuthInterceptor())
+//                .addPathPatterns("/api/**");
+//
+//        registry.addInterceptor(new ResourceAuthInterceptor())
+//                .addPathPatterns("/videos/**");
+//    }
+//}
