d04f84775fbdc969cdb1ed8a9cef6454ec625ae3
inspirit941 <inspirit941@gmail.com>
2021-03-04 20:23:03 +0900

    fix(selfQuestionController) - jwt토큰 / mapstruct 적용.

--- src/main/java/com/witherview/selfPractice/Question/SelfQuestionApiController.java
+++ src/main/java/com/witherview/selfPractice/Question/SelfQuestionApiController.java
@@ -4,10 +4,10 @@ import com.witherview.database.entity.Question;
 import com.witherview.exception.ErrorCode;
 import com.witherview.exception.ErrorResponse;
 import com.witherview.selfPractice.CustomValidator;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import com.witherview.utils.SelfQuestionMapper;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,21 +24,26 @@ import java.util.stream.Collectors;
 @Api(tags = "Question API")
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class SelfQuestionApiController {
-    private final ModelMapper modelMapper;
+    private final SelfQuestionMapper selfQuestionMapper;
     private final SelfQuestionService selfQuestionService;
     private final CustomValidator customValidator;
 
     // 질문 등록
     @ApiOperation(value="질문 등록")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="Authorization", paramType = "header")
+    })
     @PostMapping(path = "/api/self/question", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> saveQuestion(@RequestBody @Valid SelfQuestionDTO.QuestionSaveDTO requestDto,
-                                          BindingResult result,
-                                          @ApiIgnore Errors errors) {
+    public ResponseEntity<?> saveQuestion(
+            @RequestBody @Valid SelfQuestionDTO.QuestionSaveDTO requestDto,
+            BindingResult error,
+            @ApiIgnore Errors errors) {
         // requestDTO 객체 검사
-        if(result.hasErrors()) {
-            ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, result);
+        if(error.hasErrors()) {
+            ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, error);
             return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
         }
 
@@ -51,15 +56,19 @@ public class SelfQuestionApiController {
         }
 
         List<Question> lists = selfQuestionService.save(requestDto);
-        return new ResponseEntity<>(modelMapper.map(lists, SelfQuestionDTO.ResponseDTO[].class), HttpStatus.CREATED);
+        return new ResponseEntity<>(selfQuestionMapper.toResponseDtoArray(lists), HttpStatus.CREATED);
     }
 
     // 질문 수정
     @ApiOperation(value="질문 수정")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="Authorization", paramType = "header")
+    })
     @PatchMapping(path = "/api/self/question", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> updateQuestion(@RequestBody List<SelfQuestionDTO.QuestionUpdateDTO> requestDto,
-                                  @ApiIgnore Errors errors) {
+    public ResponseEntity<?> updateQuestion(
+            @RequestBody List<SelfQuestionDTO.QuestionUpdateDTO> requestDto,
+            @ApiIgnore Errors errors) {
 
         customValidator.validate(requestDto, errors);
 
@@ -68,28 +77,31 @@ public class SelfQuestionApiController {
             return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
         }
 
-        selfQuestionService.update(requestDto);
-
-        List<Question> lists = requestDto.stream()
-                .map(dto -> selfQuestionService.findQuestion(dto.getId()))
-                .collect(Collectors.toList());
-        return new ResponseEntity<>(modelMapper.map(lists, SelfQuestionDTO.ResponseDTO[].class), HttpStatus.OK);
+        var result = selfQuestionService.update(requestDto);
+        return new ResponseEntity<>(selfQuestionMapper.toResponseDtoArray(result), HttpStatus.OK);
     }
 
     // 모든 질문 조회
     @ApiOperation(value="질문 조회")
-    @GetMapping(path = "/api/self/question")
-    public ResponseEntity<?> findAllQuestion(@ApiParam(value = "조회할 질문리스트 id", required = true) @RequestParam("listId") Long listId) {
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="Authorization", paramType = "header")
+    })
+    @GetMapping(path = "/api/self/question/{id}")
+    public ResponseEntity<?> findAllQuestion(
+            @ApiParam(value = "조회할 질문리스트 id", required = true) @PathVariable("id") Long listId) {
         List<Question> lists = selfQuestionService.findAllQuestions(listId);
-        return new ResponseEntity<>(modelMapper.map(lists, SelfQuestionDTO.ResponseDTO[].class), HttpStatus.OK);
+        return new ResponseEntity<>(selfQuestionMapper.toResponseDtoArray(lists), HttpStatus.OK);
     }
 
     // 질문 삭제
     @ApiOperation(value="질문 삭제")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="Authorization", paramType = "header")
+    })
     @DeleteMapping(path = "/api/self/question/{id}")
     public ResponseEntity<?> deleteQuestion(@ApiParam(value = "삭제할 질문 id", required = true) @PathVariable Long id) {
         Question deletedQuestion = selfQuestionService.delete(id);
-        return new ResponseEntity<>(modelMapper.map(deletedQuestion, SelfQuestionDTO.DeleteResponseDTO.class), HttpStatus.OK);
+        return new ResponseEntity<>(deletedQuestion.getId(), HttpStatus.OK);
     }
 }
 
