1c48a866b0811b9e97fb08f5a2159482e1e4e7b7
inspirit941 <inspirit941@gmail.com>
2021-04-03 21:18:38 +0900

    env: OAuth와 통신할 RestTemplate 설정.

--- src/main/java/com/witherview/configuration/AppConfig.java
+++ src/main/java/com/witherview/configuration/AppConfig.java
@@ -3,11 +3,30 @@ package com.witherview.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.io.IOException;
 
 @Configuration
 public class AppConfig {
 
+    @Bean
+    public RestTemplate restTemplate() {
+        var restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+            public boolean hasError(ClientHttpResponse response) throws IOException {
+                HttpStatus statusCode = response.getStatusCode();
+                return statusCode.series() == HttpStatus.Series.SERVER_ERROR;
+            }
+        });
+        return restTemplate;
+    }
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
