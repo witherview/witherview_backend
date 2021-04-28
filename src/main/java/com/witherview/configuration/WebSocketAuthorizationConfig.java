eb19eda7f6c64d9e26a257e5011732cffcaf6d1e
inspirit941 <inspirit941@gmail.com>
2021-04-28 23:01:08 +0900

    refactor: config 일부 비활성화. 테스트완료 후 삭

--- src/main/java/com/witherview/configuration/WebSocketAuthorizationConfig.java
+++ src/main/java/com/witherview/configuration/WebSocketAuthorizationConfig.java
@@ -1,17 +1,26 @@
 package com.witherview.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.security.messaging.access.intercept.ChannelSecurityInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageSecurityMetadataSource;
 
-@Configuration
-public class WebSocketAuthorizationConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
-    @Override
-    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-        messages.anyMessage().authenticated();
-    }
+//@Configuration
+//@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+//public class WebSocketAuthorizationConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+//        messages.anyMessage().authenticated();
+//    }
+//
 //    @Override
 //    protected boolean sameOriginDisabled() {
-//        return super.sameOriginDisabled();
+//        return true;
 //    }
-}
+//}
