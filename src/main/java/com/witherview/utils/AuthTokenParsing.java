71938144567682ee99945af7f70bb2c660b3eb86
inspirit941 <inspirit941@gmail.com>
2021-04-29 10:25:01 +0900

    fix: invalid token 예외처리 로직 추가.

--- src/main/java/com/witherview/utils/AuthTokenParsing.java
+++ src/main/java/com/witherview/utils/AuthTokenParsing.java
@@ -1,13 +1,20 @@
 package com.witherview.utils;
 
+import com.witherview.account.exception.InvalidJwtTokenException;
 import com.witherview.configuration.authentication.JWSAuthenticationToken;
+import com.witherview.exception.ErrorCode;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 public class AuthTokenParsing {
     public static String getAuthClaimValue(Authentication auth, String key) {
-        if (auth instanceof JWSAuthenticationToken && auth.isAuthenticated()) {
-            return (String) auth.getCredentials();
+        if (auth instanceof JWSAuthenticationToken) {
+            if (auth.isAuthenticated()) {
+                return (String) auth.getCredentials();
+            }
+            else {
+                throw new InvalidJwtTokenException(ErrorCode.INVALID_JWT_TOKEN);
+            }
         }
         JwtAuthenticationToken token = (JwtAuthenticationToken) auth;
 //        System.out.println(token.getTokenAttributes());
