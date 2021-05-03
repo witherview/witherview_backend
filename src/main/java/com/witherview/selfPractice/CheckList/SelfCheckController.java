1abd69f06458bd3bc61d3be24307a3d1d7f012ed
MoonYeeun <aams82@hanmail.net>
2021-05-03 23:59:20 +0900

    fix: 체크리스트 api 수정

--- src/main/java/com/witherview/selfPractice/CheckList/SelfCheckController.java
+++ src/main/java/com/witherview/selfPractice/CheckList/SelfCheckController.java
@@ -4,6 +4,7 @@ import com.witherview.database.entity.SelfCheck;
 import com.witherview.exception.ErrorCode;
 import com.witherview.exception.ErrorResponse;
 import com.witherview.selfPractice.CustomValidator;
+import com.witherview.utils.AuthTokenParsing;
 import com.witherview.utils.SelfCheckMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -30,7 +31,7 @@ public class SelfCheckController {
     private final SelfCheckService selfCheckService;
     private final CustomValidator customValidator;
 
-    @ApiOperation(value="체크리스트 등록")
+    @ApiOperation(value="혼자 연습 후 셀프 체크리스트 등록")
     @ApiImplicitParams({
             @ApiImplicitParam(name="Authorization", paramType = "header")
     })
@@ -54,21 +55,22 @@ public class SelfCheckController {
             return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
         }
 
-        List<SelfCheck> selfChecks = selfCheckService.save(requestDto);
+        String userId = AuthTokenParsing.getAuthClaimValue(authentication, "userId");
+        List<SelfCheck> selfChecks = selfCheckService.save(userId, requestDto);
         return new ResponseEntity<>(selfCheckMapper.toResultArray(selfChecks), HttpStatus.CREATED);
     }
 
-    @ApiOperation(value="체크리스트 목록 조회")
+    @ApiOperation(value="기본으로 제공되는 체크리스트 질문 목 조회")
     @ApiImplicitParams({
             @ApiImplicitParam(name="Authorization", paramType = "header")
     })
     @GetMapping(path = "/api/self/checklist")
     public ResponseEntity<?> getCheckList(@ApiIgnore Authentication authentication) {
-        List<SelfCheckDTO.CheckListResponseDTO> lists = selfCheckService.findAll();
-        return new ResponseEntity<>(lists.toArray(), HttpStatus.OK);
+        List<SelfCheckDTO.CheckListResponseDTO> lists = selfCheckService.findAllCheckLists();
+        return new ResponseEntity<>(lists, HttpStatus.OK);
     }
 
-    @ApiOperation(value="혼자 연습 후 체크리스트 결과 조회")
+    @ApiOperation(value="혼자 연습내역 체크리스트 결과 조회")
     @ApiImplicitParams({
             @ApiImplicitParam(name="Authorization", paramType = "header")
     })
@@ -77,8 +79,8 @@ public class SelfCheckController {
             @PathVariable("selfHistoryId") Long selfHistoryId,
             @ApiIgnore Authentication authentication
     ) {
-        List<SelfCheck> lists = selfCheckService.findResults(selfHistoryId);
-        var result = selfCheckMapper.toResultArray(lists);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        String userId = AuthTokenParsing.getAuthClaimValue(authentication, "userId");
+        List<SelfCheck> lists = selfCheckService.findResults(userId, selfHistoryId);
+        return new ResponseEntity<>(selfCheckMapper.toResultArray(lists), HttpStatus.OK);
     }
 }
--- src/main/java/com/witherview/selfPractice/CheckList/SelfCheckService.java
+++ src/main/java/com/witherview/selfPractice/CheckList/SelfCheckService.java
@@ -4,6 +4,7 @@ import com.witherview.database.entity.*;
 import com.witherview.database.repository.*;
 import com.witherview.selfPractice.exception.NotFoundCheckList;
 import com.witherview.selfPractice.exception.NotFoundHistory;
+import com.witherview.selfPractice.exception.NotOwnedSelfHistory;
 import com.witherview.utils.SelfCheckMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class SelfCheckService {
     private final SelfHistoryRepository selfHistoryRepository;
@@ -21,11 +23,12 @@ public class SelfCheckService {
     private final CheckListRepository checkListRepository;
     private final SelfCheckMapper selfCheckMapper;
 
-
-    public List<SelfCheck> save(SelfCheckDTO.SelfCheckRequestDTO requestDto) {
+    @Transactional
+    public List<SelfCheck> save(String userId, SelfCheckDTO.SelfCheckRequestDTO requestDto) {
         SelfHistory selfHistory = selfHistoryRepository.findById(requestDto.getSelfHistoryId())
                 .orElseThrow(NotFoundHistory::new);
 
+        authenticateOwner(userId, selfHistory);
         return requestDto.getCheckLists().stream()
                 .map(dto -> {
                     findCheckList(dto.getCheckListId());
@@ -36,19 +39,24 @@ public class SelfCheckService {
                 .collect(Collectors.toList());
     }
 
-    public List<SelfCheckDTO.CheckListResponseDTO> findAll() {
+    public List<SelfCheckDTO.CheckListResponseDTO> findAllCheckLists() {
         List<CheckListType> listTypes = checkListTypeRepository.findAll();
-        return selfCheckMapper.toResponseDtoList(listTypes);
+        return listTypes.stream().map(e -> selfCheckMapper.toResponseDto(e)).collect(Collectors.toList());
     }
 
-    public List<SelfCheck> findResults(Long selfHistoryId) {
-        SelfHistory selfHistory = selfHistoryRepository.findById(selfHistoryId)
-                .orElseThrow(NotFoundHistory::new);
-
+    public List<SelfCheck> findResults(String userId, Long selfHistoryId) {
+        SelfHistory selfHistory = selfHistoryRepository.findById(selfHistoryId).orElseThrow(NotFoundHistory::new);
+        authenticateOwner(userId, selfHistory);
         return selfHistory.getSelfCheckList();
     }
 
     public CheckList findCheckList(Long id) {
         return checkListRepository.findById(id).orElseThrow(NotFoundCheckList::new);
+    }
+
+    private void authenticateOwner(String userId, SelfHistory selfHistory) {
+        if (!selfHistory.getUser().getId().equals(userId)) {
+          throw new NotOwnedSelfHistory();
+        }
     }
 }
