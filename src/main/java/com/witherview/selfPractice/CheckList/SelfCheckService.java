339c7f76b8497f75b6d803ef97d2e783a689ac20
inspirit941 <inspirit941@gmail.com>
2021-05-06 18:26:05 +0900

    fix: 혼자연습 체크리스트 저장로직 수정.

--- src/main/java/com/witherview/selfPractice/CheckList/SelfCheckService.java
+++ src/main/java/com/witherview/selfPractice/CheckList/SelfCheckService.java
@@ -1,7 +1,13 @@
 package com.witherview.selfPractice.CheckList;
 
-import com.witherview.database.entity.*;
-import com.witherview.database.repository.*;
+import com.witherview.database.entity.CheckList;
+import com.witherview.database.entity.CheckListType;
+import com.witherview.database.entity.SelfCheck;
+import com.witherview.database.entity.SelfHistory;
+import com.witherview.database.repository.CheckListRepository;
+import com.witherview.database.repository.CheckListTypeRepository;
+import com.witherview.database.repository.SelfCheckRepository;
+import com.witherview.database.repository.SelfHistoryRepository;
 import com.witherview.selfPractice.exception.NotFoundCheckList;
 import com.witherview.selfPractice.exception.NotFoundHistory;
 import com.witherview.utils.SelfCheckMapper;
@@ -9,11 +15,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class SelfCheckService {
     private final SelfHistoryRepository selfHistoryRepository;
     private final SelfCheckRepository selfCheckRepository;
@@ -21,26 +29,30 @@ public class SelfCheckService {
     private final CheckListRepository checkListRepository;
     private final SelfCheckMapper selfCheckMapper;
 
-
-    public List<SelfCheck> save(SelfCheckDTO.SelfCheckRequestDTO requestDto) {
+    @Transactional
+    public List<SelfCheck> save(SelfCheckDTO.SelfCheckRequestDTO requestDto, String userId) {
         SelfHistory selfHistory = selfHistoryRepository.findById(requestDto.getSelfHistoryId())
                 .orElseThrow(NotFoundHistory::new);
+        if (!selfHistory.getUser().getId().equals(userId)) {
+            throw new NotFoundHistory();
+        }
 
-        return requestDto.getCheckLists().stream()
-                .map(dto -> {
-                    findCheckList(dto.getCheckListId());
-                    SelfCheck selfCheck = selfCheckMapper.toSelfCheckEntity(dto);
-                    selfHistory.addSelfCheck(selfCheck);
-                    return selfCheckRepository.save(selfCheck);
-                })
-                .collect(Collectors.toList());
+        // 혼자연습의 체크리스트 영역 덮어쓰기. (중복저장 문제 해결을 위해)
+//        selfHistory.setSelfCheckList(new ArrayList<>());
+        List<SelfCheck> result = new ArrayList<>();
+        for (var data: requestDto.getCheckLists()) {
+            SelfCheck selfCheck = selfCheckMapper.toSelfCheckEntity(data);
+            selfHistory.addSelfCheck(selfCheck);
+        }
+        return selfHistory.getSelfCheckList();
     }
 
-    public List<SelfCheckDTO.CheckListResponseDTO> findAll() {
-        List<CheckListType> listTypes = checkListTypeRepository.findAll();
-        return selfCheckMapper.toResponseDtoList(listTypes);
-    }
+//    public List<SelfCheckDTO.CheckListResponseDTO> findAll(String userId) {
+//        List<CheckListType> listTypes = checkListTypeRepository.findAll();
+//        return selfCheckMapper.toResponseDtoList(listTypes);
+//    }
 
+    @Transactional
     public List<SelfCheck> findResults(Long selfHistoryId) {
         SelfHistory selfHistory = selfHistoryRepository.findById(selfHistoryId)
                 .orElseThrow(NotFoundHistory::new);
