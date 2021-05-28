3494d6f763df80bd9240305b0a0ab06ac414d37f
MoonYeeun <aams82@hanmail.net>
2021-05-29 01:22:09 +0900

    fix : SelfHistory와 QuestionList 연관관계 삭제

--- src/main/java/com/witherview/utils/SelfHistoryMapper.java
+++ src/main/java/com/witherview/utils/SelfHistoryMapper.java
@@ -13,17 +13,4 @@ public interface SelfHistoryMapper {
     SelfHistoryDTO.SelfHistoryDefaultResponseDTO toResponseDto(SelfHistory selfHistory);
 
     SelfHistoryDTO.SelfHistoryResponseDTO[] toResponseArray(List<SelfHistory> selfHistoryList);
-
-    default SelfHistoryResponseDTO toResponseArray(SelfHistory selfHistory) {
-        SelfHistoryResponseDTO selfHistoryResponseDTO = new SelfHistoryResponseDTO();
-
-        selfHistoryResponseDTO.setId(selfHistory.getId());
-        selfHistoryResponseDTO.setQuestionListId(selfHistory.getQuestionList().getId());
-        selfHistoryResponseDTO.setQuestionListEnterprise(selfHistory.getQuestionList().getEnterprise());
-        selfHistoryResponseDTO.setQuestionListJob(selfHistory.getQuestionList().getJob());
-        selfHistoryResponseDTO.setSavedLocation(selfHistory.getSavedLocation());
-        selfHistoryResponseDTO.setCreatedAt(selfHistory.getCreatedAt());
-
-        return selfHistoryResponseDTO;
-    }
 }
