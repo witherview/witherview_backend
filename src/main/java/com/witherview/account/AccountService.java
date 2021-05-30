1207e8ca65faeec42d1e3acab4704a1191d37076
inspirit941 <inspirit941@gmail.com>
2021-05-30 16:46:38 +0900

    fix: profileImg 문자열에 s3 링크 직접 제공 - 추후 변경 필요함.

--- src/main/java/com/witherview/account/AccountService.java
+++ src/main/java/com/witherview/account/AccountService.java
@@ -67,7 +67,7 @@ public class AccountService {
     private String grantType;
     @Value("${spring.security.oauth2.client.provider.witherview.token-uri}")
     private String tokenUri;
-    @Value("${application.bucket.name}")
+    @Value("${application.bucket.profile}")
     private String bucketName;
 
 
@@ -137,6 +137,7 @@ public class AccountService {
                     new PutObjectRequest(bucketName, profileName, newImg)
             );
             newImg.delete();
+            // todo: s3 링크를 그냥 줄지, 클라우드프론트 등의 작업을 할지 / 내부 api로 대체할지 고민해야 함.
             var url = s3Client.getUrl(bucketName, profileName).toString();
             user.uploadImg(url);
         } catch(Exception e) {
