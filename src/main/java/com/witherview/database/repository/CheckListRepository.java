073cf35373cd5c041089c2db6702462387642221
MoonYeeun <aams82@hanmail.net>
2021-06-12 03:38:56 +0900

    chore: 불필요한 코드 삭제

--- src/main/java/com/witherview/database/repository/CheckListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.witherview.database.repository;
-
-import com.witherview.database.entity.CheckList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CheckListRepository extends JpaRepository<CheckList, Long> {
-}
--- src/main/java/com/witherview/database/repository/CheckListTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.witherview.database.repository;
-
-import com.witherview.database.entity.CheckListType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CheckListTypeRepository extends JpaRepository<CheckListType, Long> {
-}
