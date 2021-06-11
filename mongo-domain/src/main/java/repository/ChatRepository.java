46827c251a89aa369593c0a17099bebdfdf6bfba
MoonYeeun <aams82@hanmail.net>
2021-06-11 23:06:40 +0900

    env: mongo-module 분리

---
+++ mongo-domain/src/main/java/repository/ChatRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import entity.Chat;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ChatRepository extends MongoRepository<Chat, Long> {
+    Page<Chat> findAllByStudyRoomId(Long studyRoomId, Pageable pagable);
+}
