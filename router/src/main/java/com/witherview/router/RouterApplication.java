585d2f7ad8ce4c160a86c033acff9525dc002ce6
MoonYeeun <aams82@hanmail.net>
2021-06-24 15:51:17 +0900

    feat: springApplication 추가

---
+++ router/src/main/java/com/witherview/router/RouterApplication.java
@@ -0,0 +1,11 @@
+package com.witherview.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(RouterApplication.class, args);
+  }
+}
