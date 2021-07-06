8f28c7c403390489a11fcbb098dfccc475e7d3e2
inspirit941 <inspirit941@gmail.com>
2021-07-07 08:37:02 +0900

    fix: mapper에 length 필드 추가.

--- study-api/src/main/java/com/witherview/study/mapper/SelfQuestionListMapper.java
+++ study-api/src/main/java/com/witherview/study/mapper/SelfQuestionListMapper.java
@@ -4,13 +4,16 @@ import com.witherview.mysql.entity.QuestionList;
 import com.witherview.study.dto.SelfQuestionListDTO;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface SelfQuestionListMapper {
 
     QuestionList toQuestionList(SelfQuestionListDTO.QuestionListSaveDTO questionListSaveDTO);
+    @Mapping(expression = "java(questionList.getQuestions().size())", target = "length")
     SelfQuestionListDTO.ResponseDTO toResponseDto(QuestionList questionList);
+
     SelfQuestionListDTO.ResponseDTO[] toResponseDtoArray(List<QuestionList> questionListList);
     SelfQuestionListDTO.DeleteResponseDTO toDeleteDTo(QuestionList questionList);
 }
